c132a5fe9dedd09187decd3edd192f32a7c2af70
Soney <sonephyo7777777@gmail.com>
2025-04-14 12:32:09 -0400

    config: finishing up previous commit

--- BitezoneBackend/settings/local.py
+++ BitezoneBackend/settings/local.py
@@ -63,7 +63,7 @@ ACCOUNT_SIGNUP_FIELDS = ["username", "email*", "password1*", "password2*"]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
-        'rest_framework.authentication.SessionAuthentication',
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAdminUser",),
 }
