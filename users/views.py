a505a3663a90a4384ba398bf78eb4d175c0a7105
Soney <sonephyo7777777@gmail.com>
2025-04-14 12:31:16 -0400

    fix: adding permission allowany to prevent invalid crenditals when returning and using customLogin for comptability for google

--- users/views.py
+++ users/views.py
@@ -1,26 +1,54 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
-
-from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 import urllib
 
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
-class GoogleLogin(
-    SocialLoginView
-):  # if you want to use Authorization Code Grant, use this
+
+class CustomGoogleOAuth2Client(OAuth2Client):
+    def __init__(
+        self,
+        request,
+        consumer_key,
+        consumer_secret,
+        access_token_method,
+        access_token_url,
+        callback_url,
+        _scope,  # This is fix for incompatibility between django-allauth==65.3.1 and dj-rest-auth==7.0.1
+        scope_delimiter=" ",
+        headers=None,
+        basic_auth=False,
+    ):
+        super().__init__(
+            request,
+            consumer_key,
+            consumer_secret,
+            access_token_method,
+            access_token_url,
+            callback_url,
+            scope_delimiter,
+            headers,
+            basic_auth,
+        )
+
+
+class GoogleLogin(SocialLoginView):
     class GoogleAdapter(GoogleOAuth2Adapter):
         access_token_url = "https://oauth2.googleapis.com/token"
         authorize_url = "https://accounts.google.com/o/oauth2/v2/auth"
         profile_url = "https://www.googleapis.com/oauth2/v2/userinfo"
 
     adapter_class = GoogleAdapter
-    callback_url = "http://localhost:8000/REST/v1/code"
-    client_class = OAuth2Client
+    callback_url = "http://localhost:8000/users/code"
+    client_class = CustomGoogleOAuth2Client
 
 
 @api_view(["GET"])
+@permission_classes([AllowAny])
 def CodeView(request):
     """
     List all code snippets, or create a new snippet.
